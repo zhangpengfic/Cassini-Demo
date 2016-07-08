04ec37cec4355be60b9ced0b0449fd2495e8251f
Alfred <zhangpeng.fic@163.com>
2016-07-08 10:13:30 +0800

    load to Master

---
+++ Cassini/DemoURL.swift
@@ -0,0 +1,18 @@
+//
+//  DemoURL.swift
+//  Cassini
+//
+//  Created by Alberto Vega Gonzalez on 9/13/15.
+//  Copyright (c) 2015 Alberto Vega Gonzalez. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL {
+  static let Stanford = NSURL(string:"http://imgstore.cdn.sogou.com/app/a/100540002/714860.jpg") //"http://comm.stanford.edu/wp-content/uploads/2013/01/stanford-campus.png")
+  struct NASA {
+  static let Cassini = NSURL(string: "http://pic24.nipic.com/20121003/10754047_140022530392_2.jpg")//"http://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg")
+  static let Earth = NSURL(string: "http://img.taopic.com/uploads/allimg/140326/235113-1403260I33562.jpg")// "http://www.nasa.gov/sites/default/files/wave_earth_mosaic_3.jpg")
+    static let Saturn = NSURL(string: "http://image.tianjimedia.com/uploadImages/2012/011/R5J8A0HYL5YV.jpg")// "http://www.nasa.gov/sites/default/files/saturn_collage.jpg")
+  }
+}
---
+++ Cassini/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Cassini
+//
+//  Created by Alberto Vega Gonzalez on 9/11/15.
+//  Copyright (c) 2015 Alberto Vega Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if let ivc = segue.destinationViewController as? ImageViewController {
+      if let identifier = segue.identifier {
+        switch identifier {
+          case "Earth":
+          ivc.imageURL = DemoURL.NASA.Earth
+          ivc.title = "Earth"
+          case "Saturn":
+          ivc.imageURL = DemoURL.NASA.Saturn
+          ivc.title = "Saturn"
+          case "Cassini":
+          ivc.imageURL = DemoURL.NASA.Cassini
+          ivc.title = "Cassini"
+        default: break
+        }
+      }
+    }
+  }
+
+}
+
